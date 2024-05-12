96b0d728819e2c2a3a23b9ace85dfee7209383dd
CrazyTweeks <bhuvanbm7@gmail.com>
2024-05-12 11:17:43 +0530

    chore: Update @types/react to version 18.3.2

--- components/appLayout.tsx
+++ components/appLayout.tsx
@@ -1,20 +1,14 @@
-import { FC, PropsWithChildren, cache } from "react";
+import { FC, PropsWithChildren } from "react";
 import { Navbar } from "@/components/navbar/navbar";
 // import MobileBottomNav from "./navbar/mobileBottomNav";
 
 export const revalidate = 30;
 
-const getInitialData = cache(async () => {
-  return { isAuthenticated: false, user: undefined };
-}); //TODO: USER AUTH MOCKING IN DEVELOPMENT
-
 const AppLayout: FC<PropsWithChildren> = async ({ children }) => {
-  const { isAuthenticated, user } = await getInitialData();
-
   return (
     <div>
       <div className="relative flex  flex-col">
-        <Navbar isAuthenticated={isAuthenticated} user={user} />
+        <Navbar isAuthenticated={false} user={undefined} />
         <main className="md:container md:mx-auto md:px-2 p-0 m-0 min-w-full">
           {children}
         </main>
